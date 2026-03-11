85ab37befb4ec0d3003ba11448ee5f1054d232a6
Yugant <ynagrala@iu.edu>
2026-03-11 17:02:52 -0400

    Exercise 2 Complete

--- apputil.py
+++ apputil.py
@@ -18,3 +18,26 @@ class MarkovText:
         
         self.term_dict = dict(term_dict)
         return self.term_dict
+    
+    def generate(self, term_count=20, seed_term=None):
+        if self.term_dict is None:
+            self.get_term_dict()
+        
+        if seed_term is None:
+            current_word = np.random.choice(list(self.term_dict.keys()))
+        else:
+            if seed_term not in self.term_dict:
+                raise ValueError(f"Seed term '{seed_term}' not found in corpus")
+            current_word = seed_term
+        
+        result = [current_word]
+        
+        for _ in range(term_count - 1):
+            if current_word not in self.term_dict or len(self.term_dict[current_word]) == 0:
+                current_word = np.random.choice(list(self.term_dict.keys()))
+            else:
+                current_word = np.random.choice(self.term_dict[current_word])
+            
+            result.append(current_word)
+        
+        return ' '.join(result)
